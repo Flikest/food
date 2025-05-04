0d155db1b412307be9c32c444c9387c01a8fbd4d
muteys <1hersil88@gmail.com>
2025-05-04 12:36:35 +0600

    Added __init__

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 import telebot
-
+from api.api import find_places
 load_dotenv()
 user_data = {} #dict for users {'chat_id': '_user_status'}
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
@@ -12,14 +12,14 @@ def welcome(message):
     #TODO: Check if user in database
     #True
     bot.send_message(chat_id, 'Здравствуйте, отправьте название блюда, которое вы хотели бы попробовать.')
-    #user_data[chat_id] = '_is_writing_dish'
+    user_data[chat_id] = '_is_writing_dish'
     #False
-    bot.send_message(chat_id, 'Здравствуйте, это бот для поиска где и с кем вы можете поесть ту еду, которую вы выбрали.')
-    bot.send_message(chat_id, 'Давайте создадим вам анкету.')
-    bot.send_message(chat_id, 'Введите свой пол:')
-    user_date[char_id] = '_is_writing_sex'
+    #bot.send_message(chat_id, 'Здравствуйте, это бот для поиска где и с кем вы можете поесть ту еду, которую вы выбрали.')
+    #bot.send_message(chat_id, 'Давайте создадим вам анкету.')
+    #bot.send_message(chat_id, 'Введите свой пол:')
+    #user_data[char_id] = '_is_writing_sex'
 
-@bot.message_handler(func = lambda message: True):
+@bot.message_handler(func = lambda message: True)
 def handle_message(message):
     chat_id = message.chat.id
     if user_data[chat_id] == '_is_writing_sex':
@@ -41,15 +41,15 @@ def handle_message(message):
         #TODO: add the description in sql tablet
 
         bot.send_message(chat_id, 'Поздравляю, вы были зарегестрированы, теперь напишите команду /start еще раз.')
-    if user_data[chat_id] = '_is_writing_dish':
-        #TODO: Make api request and processing an api response
-        
-        #Show restorance (mb in button to make them clickable) and other users
-        user_data[chat_id] = '_is_selecting_restaurant'
-
-
-
-
-
+    if user_data[chat_id] == '_is_writing_dish':
+        dish = message.text
+        user_data[chat_id] = '_is_sending_geo'
+        bot.send_message(chat_id, 'Пожалуйста, отправьте свою геолокацию.')
+    if user_data[chat_id] == '_is_sending_geo' and message.location is not None:
+        response = find_places(message.location ,dish)
+        print(1)
+        user_data[chat_id] = '_is_selecting_restaurance'
+    if user_data[chat_id] == '_is_selecting_restaurance':
+        bot.send_message(chat_id, 'Ресторан ))')
 bot.infinity_polling()
 
